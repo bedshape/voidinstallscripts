f969bd5cdf101ecefde4ad34363924ee6f7ef88b
bedshape <rebis@sent.com>
2025-01-15 21:30:46 +0800

    Delete installscripts directory

--- installscripts/installgrub.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id="void" --recheck --modules="tpm" --disable-shim-lock && grub-mkconfig -o /boot/grub/grub.cfg
-
-echo "remember to generate initramfs (xbps-reconfigure -fa)"
--- installscripts/vols.sh
+++
@@ -1,8 +0,0 @@
-pvcreate /dev/mapper/rbs
-vgcreate vols /dev/mapper/rbs
-lvcreate -L 4G -n swap vols && mkswap /dev/vols/swap 
-lvcreate -L 1G -n boot vols && mkfs.ext4 /dev/vols/boot
-lvcreate -l 100%FREE -n root vols && mkfs.ext4 /dev/vols/root
-
-
-
