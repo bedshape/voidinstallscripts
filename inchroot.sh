17b0c14d53ac3ff43a5f057736aff34fe510c356
bedshape <rebis@sent.com>
2025-01-16 13:17:47 +0800

    Update inchroot.sh

--- inchroot.sh
+++ inchroot.sh
@@ -1,6 +1,6 @@
 read -p "enter locale: " locale 
 read -p "enter rbs_block: " rbs_block 
-read -p "enter hostname: " hostname
+read -p "enter root/boot/swap blockname (/dev/xyz2): " hostname
 
 echo "$hostname" > /etc/hostname 
 
